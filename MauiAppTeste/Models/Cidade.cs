8cd6e55e114f4d98d395341fe00dbab0e29d3f1d
Beatriz Pinheiro <101609977+Biapinheiroac@users.noreply.github.com>
2024-05-16 14:44:29 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTeste/Models/Cidade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppTeste.Models
+{
+    public class Cidade
+    {
+        public int id_cidade { get; set; }
+
+        public string descricao { get; set; }
+
+        public string uf { get; set; }
+
+        public int codigo_ibg { get; set; }
+
+        public string ddd { get; set; }
+    }
+}
---
+++ MauiAppTeste/Models/Logradouro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppTeste.Models
+{
+    public class Logradouro
+    {
+        public int CEP { get; set; }
+
+        public string id_logradouro { get; set; }
+
+        public string tipo { get; set; }
+
+        public int descricao { get; set; }
+
+        public string id_cidade { get; set; }
+
+        public int UF { get; set; }
+
+        public string complemento { get; set; }
+
+        public string descricao_sem_numero { get; set; }
+
+        public int descricao_cidade { get; set; }
+
+        public string codigo_cidade_ibge { get; set; }
+
+        public string descricao_bairro { get; set; }
+    }
+}
